3cbde90fcd9f48b2b790724652b5b7568efe2e6b
jason416 <jason416@foxmail.com>
2019-03-29 00:12:12 +0800

    add two devices <-> two cdev obj; only one driver

--- cdev/vserial.c
+++ cdev/vserial.c
@@ -17,19 +17,29 @@
 
 #define VSERIAL_MAJOR		256
 #define VSERIAL_MINOR		0
-#define VSERIAL_DEV_CNT		1
+#define VSERIAL_DEV_CNT		2
 #define VSERIAL_DEV_NAME	"vserial"
 #define KFIFO_SIZE			32
 
-/* cdev object */
-static struct cdev vser_cdev;
+/* new class, derived from cdev class */
+struct vser_dev {
+	struct kfifo *fifo;		/* private data */
+	struct cdev cdev;		/* base class */
+};
 
-/* define a kfifo object, which has 32 elements, type char */
-DEFINE_KFIFO(vser_fifo, char, KFIFO_SIZE);
+static struct vser_dev vser_dev[VSERIAL_DEV_CNT];
+
+/* define two kfifo object, which has 32 elements, type char; each one for a vser_dev*/
+static DEFINE_KFIFO(vser_fifo0, char, KFIFO_SIZE);
+static DEFINE_KFIFO(vser_fifo1, char, KFIFO_SIZE);
 
 /********** define operations for VFS ************/
 static int vser_open(struct inode *inode, struct file *filp)
 {
+	/* inode will record this dev's cdev, like inode::i_cdev -> vser_dev::cdev,
+	 * each devices has diffrent cdev struct.
+	 */
+	filp->private_data = container_of(inode->i_cdev, struct vser_dev, cdev);
 	return 0;
 }
 
@@ -42,9 +52,12 @@ static ssize_t vser_read(struct file *filp, char __user *buf, size_t count, loff
 {
 	int ret = 0;
 	ssize_t copied = 0;
+
+	/* get this device's object addr */
+	struct vser_dev *dev = filp->private_data;
 	
 	/* copy kfifo's content to user space */
-	ret = kfifo_to_user(&vser_fifo, buf, count > KFIFO_SIZE ? KFIFO_SIZE : count, &copied);
+	ret = kfifo_to_user(dev->fifo, buf, count > KFIFO_SIZE ? KFIFO_SIZE : count, &copied);
 	if(ret != 0)
 		return ret;
 	else
@@ -56,8 +69,11 @@ static ssize_t vser_write(struct file *filp, const char __user *buf, size_t coun
 	int ret = 0;
 	ssize_t copied = 0;
 
+	/* get this device's object addr */
+	struct vser_dev *dev = filp->private_data;
+
 	/* copy user space's content to kfifo */
-	ret = kfifo_from_user(&vser_fifo, buf, count > KFIFO_SIZE ? KFIFO_SIZE : count, &copied);
+	ret = kfifo_from_user(dev->fifo, buf, count > KFIFO_SIZE ? KFIFO_SIZE : count, &copied);
 	if(ret != 0)
 		return ret;
 	else
@@ -75,7 +91,7 @@ static struct file_operations vser_ops = {
 
 static int __init vser_init(void)
 {
-	int ret;
+	int i, ret;
 	dev_t dev;
 	
 	/* make a dev_t, and register */
@@ -84,19 +100,24 @@ static int __init vser_init(void)
 	if(ret)
 		goto reg_err;
 
-	/* bind cdev object with file_ops */
-	cdev_init(&vser_cdev, &vser_ops);
-	vser_cdev.owner = THIS_MODULE;
-	
-	/* after cdev object initializaton over, add to cdev_map */
-	ret = cdev_add(&vser_cdev, dev, VSERIAL_DEV_CNT);
-	if(ret)
-		goto add_err;
+	/* bind cdev object with file_ops, then add to cdev_map */
+	for(i = 0; i < VSERIAL_DEV_CNT; i++) {
+		cdev_init(&vser_dev[i].cdev, &vser_ops);
+		vser_dev[i].cdev.owner = THIS_MODULE;
+		vser_dev[i].fifo = i ? (struct kfifo *)&vser_fifo1 : (struct kfifo *)&vser_fifo0; /* compatible pointer, -> start addr of struct */
 
+		/* each device has a cdev object */
+		ret = cdev_add(&vser_dev[i].cdev, dev + i, 1);
+		if(ret)
+			goto add_err;
+	}
+	
 	return 0;
 
 add_err:
-	/* if add faild, unregister */
+	/* if add faild, del cdev from cdev_map, then unregister */
+	for(--i ; i >= 0; i--)
+		cdev_del(&vser_dev[i].cdev);
 	unregister_chrdev_region(dev, VSERIAL_DEV_CNT);
 
 reg_err:
@@ -105,12 +126,14 @@ reg_err:
 
 static void __exit vser_exit(void)
 {
+	int i;
 	dev_t dev;
 	
 	dev = MKDEV(VSERIAL_MAJOR, VSERIAL_MINOR);
 
 	/* delete cdev object, and unregister */
-	cdev_del(&vser_cdev);
+	for(i = 0; i < VSERIAL_DEV_CNT; i++)
+		cdev_del(&vser_dev[i].cdev);
 	unregister_chrdev_region(dev, VSERIAL_DEV_CNT);
 }
 
