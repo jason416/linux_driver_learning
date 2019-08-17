4fe91456f734c94fe08ed75d164e0428c9613e4f
jason416 <jason416@foxmail.com>
2019-08-18 00:51:52 +0800

    fix compatible not match error; and add comment

--- cdev/platform/keys/basic/key_drv.c
+++ cdev/platform/keys/basic/key_drv.c
@@ -20,6 +20,7 @@
 struct resource *key_home_res;
 struct resource *key_back_res;
 
+/* interrupt handler */
 static irqreturn_t key_handler(int irq, void *dev_id)
 {
 	if(irq == key_home_res->start)
@@ -34,6 +35,7 @@ static int key_probe(struct platform_device *pdev)
 {
 	int ret;
 
+	/* get platform resource, type IORESOURCE_IRQ */
 	key_home_res = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
 	key_back_res = platform_get_resource(pdev, IORESOURCE_IRQ, 1);
 
@@ -42,6 +44,7 @@ static int key_probe(struct platform_device *pdev)
 		goto res_err;
 	}
 
+	/* register irq in kernel */
 	ret = request_irq(key_home_res->start, key_handler, key_home_res->flags & IRQF_TRIGGER_MASK, "key_home", NULL);
 	if(ret)
 		goto key_home_err;
@@ -60,20 +63,24 @@ res_err:
 
 static int key_remove(struct platform_device *pdev)
 {
+	/* unregister irq */
 	free_irq(key_back_res->start, NULL);
 	free_irq(key_home_res->start, NULL);
 
 	return 0;
 }
 
+/* match node in devicetree */
 static const struct of_device_id key_of_matches[] = {
-	{ .compatible = "itop4412,key", },
+	/* compatible mst be exactly the same as the node in devicetree, EVEN SPACE!!! */
+	{ .compatible = "itop4412, key", },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, key_of_matches);
 
 struct platform_driver key_drv = {
 	.driver = {
+		/* must exactly the same as the 'compatible' property of node in devicetree */
 		.name = "key",
 		.owner = THIS_MODULE,
 		.of_match_table = of_match_ptr(key_of_matches),
